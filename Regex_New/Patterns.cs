45ef620ab87acfd1067142d6f2c162c31d30703e
shubhampachghare <shubhampachghare@gmail.com>
2023-07-18 14:39:43 +0530

    [Shubham] -RegexExamples

--- Regex_New/Patterns.cs
+++ Regex_New/Patterns.cs
@@ -10,9 +10,14 @@ namespace Regex_New
     class Patterns
     {
         public static string Regex_FName = "^[A-Z]{1}[a-z]{2,}$";
+        public static string Regex_LName = "^[A-Z]{1}[a-z]{2,}$";
         public bool ValidateFName(string FName)
         {
             return Regex.IsMatch(FName, Regex_FName);
+        }
+        public bool ValidateLName(string LName)
+        {
+            return Regex.IsMatch(LName, Regex_LName);
         }
     }
 }
--- Regex_New/Program.cs
+++ Regex_New/Program.cs
@@ -6,6 +6,7 @@
         {
             Patterns patterns = new Patterns();
             Console.WriteLine(patterns.ValidateFName("Shubham"));
+            Console.WriteLine(patterns.ValidateFName("Pachghare"));
 
 
         }
