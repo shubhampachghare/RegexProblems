1f54cd4d7c2ae48fa0187d3420b63f22e0541894
shubhampachghare <shubhampachghare@gmail.com>
2023-07-18 14:37:42 +0530

    [Shubham] -RegexExamples

---
+++ Regex_New/Patterns.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_New
+{
+    class Patterns
+    {
+        public static string Regex_FName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFName(string FName)
+        {
+            return Regex.IsMatch(FName, Regex_FName);
+        }
+    }
+}
---
+++ Regex_New/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Regex_New
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Patterns patterns = new Patterns();
+            Console.WriteLine(patterns.ValidateFName("Shubham"));
+
+
+        }
+    }
+}
